2a1fc5d3e208f20477237d33e7d7b295c2797ddf
zhangpengfei <18409271159@163.com>
2024-07-09 14:14:08 +0800

    fix: :bug: change router

--- src/router/modules/dashboard.ts
+++ src/router/modules/dashboard.ts
@@ -17,9 +17,6 @@ export default [
       {
         path: "/about",
         name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (About.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("@/views/AboutView.vue")
       }
     ]
