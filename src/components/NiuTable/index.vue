d3ceda5de5ad52d0a0025ac2613b0db2805011f0
zhangpengfei <18409271159@163.com>
2024-07-26 17:28:59 +0800

    feat: 🌈 add niu-table component

---
+++ src/components/NiuTable/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <vxe-table
+      :border="true"
+      show-overflow
+      show-header-overflow
+      auto-resize
+      :row-config="{ isHover: true }"
+      :data="tableData"
+    >
+      <template v-for="item in columns" :key="item.id">
+        <vxe-column :field="item.field" :title="item.title" />
+      </template>
+    </vxe-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  tableData: {
+    type: Array<any>,
+    default: () => []
+  },
+  columns: {
+    type: Array<any>,
+    default: () => []
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/table/vxe-table/index.vue
+++ src/views/table/vxe-table/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>vxe-table</div>
+  <div>
+    <NiuTable :tableData="tableData" :columns="columns"></NiuTable>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const tableData = [
+  { id: 1, name: "锤石", position: "辅助", number: "5个" },
+  { id: 2, name: "金克斯", position: "ADC", number: "8个" },
+  { id: 3, name: "蛮王", position: "上单", number: "12个" }
+];
+const columns = [
+  { id: 1, title: "名称", field: "name", width: "auto" },
+  { id: 2, title: "位置", field: "position", width: "auto" },
+  { id: 3, title: "皮肤数量", field: "number", width: "auto" }
+];
+</script>
 
 <style scoped></style>
