6b2eff8e8235ae172ffec846b97f3170e15edc3a
zhangpengfei <18409271159@163.com>
2024-07-30 17:41:16 +0800

    feat: vxe-table comp insert operate

--- src/components/NiuTable/index.vue
+++ src/components/NiuTable/index.vue
@@ -9,6 +9,15 @@
       :data="tableData"
     >
       <template v-for="item in columns" :key="item.id">
+        <template v-if="item.field === 'operate'">
+          <vxe-column :field="item.field" :title="item.title">
+            <template #default="scope">
+              <div class="niube-operate">
+                <slot name="operate" :scope="scope"></slot>
+              </div>
+            </template>
+          </vxe-column>
+        </template>
         <vxe-column :field="item.field" :title="item.title" />
       </template>
     </vxe-table>
--- src/views/table/vxe-table/index.vue
+++ src/views/table/vxe-table/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <NiuTable :tableData="tableData" :columns="columns"></NiuTable>
+    <NiuTable :tableData="tableData" :columns="columns">
+      <template #operate="scope">
+        <template v-for="item in listOperation(scope.scope.row)" :key="item.id">
+          <span :class="item.className" @click="item.func">{{ item.title }}</span>
+        </template>
+      </template>
+    </NiuTable>
   </div>
 </template>
 
@@ -13,8 +19,32 @@ const tableData = [
 const columns = [
   { id: 1, title: "名称", field: "name", width: "auto" },
   { id: 2, title: "位置", field: "position", width: "auto" },
-  { id: 3, title: "皮肤数量", field: "number", width: "auto" }
+  { id: 3, title: "皮肤数量", field: "number", width: "auto" },
+  { id: 4, title: "操作", fixed: "rigth", field: "operate", width: "auto" }
 ];
+
+const listOperation = (row: any) => {
+  return [
+    {
+      id: 1,
+      className: "table-reload-examine",
+      title: "详情",
+      func: (e: any) => {}
+    },
+    {
+      id: 2,
+      className: "table-reload-examine",
+      title: "编辑",
+      func: (e: any) => {}
+    },
+    {
+      id: 3,
+      className: "table-reload-examine",
+      title: "删除",
+      func: (e: any) => {}
+    }
+  ];
+};
 </script>
 
 <style scoped></style>
